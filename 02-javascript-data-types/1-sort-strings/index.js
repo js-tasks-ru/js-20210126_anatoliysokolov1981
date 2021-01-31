28a718c6c6f7a0094c5b7b01ccbf6747319a9653
anatoly <anatoliysokolov1981@gmail.com>
2021-01-31 20:44:49 +0300

    sortString function implementation

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,16 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const arrCopy = [...arr];
+  const sortAscending = (str1, str2) => str1.localeCompare(str2, 'ru', {caseFirst: 'upper'});
+  const sortDescending = (str1, str2) => str2.localeCompare(str1, 'ru', {caseFirst: 'upper'});  
+  
+  if (param === 'asc') {
+    arrCopy.sort(sortAscending);
+  } else {
+    arrCopy.sort(sortDescending);
+  }
 
+  return arrCopy;
 }
+
